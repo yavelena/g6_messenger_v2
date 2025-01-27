355f1b52fd5f375f2830938240515b7355475fa1
MURZIK\yavel <11>
2025-01-27 23:14:21 +0200

    New message and Recipients windows added

--- js/index.js
+++ js/index.js
@@ -1,12 +1,46 @@
-window.addEventListener('load', () => {
-    const popup = document.getElementById('register-popup-wnd');
-    const closeButton = document.getElementById('popupClose');
+// window.addEventListener('load', () => {
+//     const popup = document.getElementById('register-popup-wnd');
+//     const closeButton = document.getElementById('popupClose');
 
-    // Показываем окно
-    popup.classList.add('active');
+//     popup.classList.add('active');
 
    
-    closeButton.addEventListener('click', () => {
-        popup.classList.remove('active');
+//     closeButton.addEventListener('click', () => {
+//         popup.classList.remove('active');
+//     });
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // Open Register Window
+    const register_popup_wnd = document.getElementById('register-popup');
+    register_popup_wnd.classList.add('active');
+
+    // Get all elements with the class 'popup-close'
+    const closeButtons = document.querySelectorAll('.popup-close');
+
+    closeButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            // Find the closest parent element with the class 'popup-overlay'
+            const popup = button.closest('.popup-overlay');
+            if (popup) {
+                popup.classList.remove('active'); // Hide the popup
+            }
+        });
     });
+
+    // Write a new message
+    const newMsgBtn = document.getElementById('new-msg-btn');
+    newMsgBtn.addEventListener('click', () => {
+        const newMsgPopup = document.getElementById('new-msg-popup');
+        newMsgPopup.classList.add('active');
+    });
+
+    const addRecipientBtn = document.getElementById('add-recipient-btn');
+    addRecipientBtn.addEventListener('click', () => {
+        const newRecipientPopup = document.getElementById('add-recipient-popup');
+        newRecipientPopup.classList.add('active');
+    });
+
+
 });
